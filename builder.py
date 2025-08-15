56d067683bd104adde86493301919b585b2d870a
ZhiChao Liang <59919798+lzc-smu@users.noreply.github.com>
2025-08-15 14:16:36 +0800

    Update builder.py

--- builder.py
+++ builder.py
@@ -1,10 +1,3 @@
-# --------------------------------------------------------
-# Dual Octree Graph Networks
-# Copyright (c) 2022 Microsoft
-# Licensed under The MIT License [see LICENSE for details]
-# Written by Peng-Shuai Wang
-# --------------------------------------------------------
-
 import ocnn
 
 import datasets
